c73b9cabf33c373dd65cde19c7ab3ee93f51c747
viskjha <vishaljha511@gmail.com>
2019-06-28 20:31:13 +0530

    Insert Form Data to Database

--- signup.php
+++ signup.php
@@ -30,47 +30,107 @@
 
     <?php 
         $fnameErr=$lnameErr=$unameErr=$emailErr=$numErr=$pass1Err=$pass2Err="";
+        $fname=$lname=$uname=$email=$num=$pass1=$pass2="";
+        $status=true;
+        $msg="";
+
         if(!empty($_POST))
         {
             if(empty($_POST['fname']))
             {
                 $fnameErr = "First Name must be Filled";
+                $status=false;
+            }
+            else {
+                $fname=$_POST['fname'];
             }
 
             if(empty($_POST['lname']))
             {
                 $lnameErr = "Last Name must be Filled";
+                $status=false;
+            }
+            else {
+                $lname = $_POST['lname'];
             }
 
             if(empty($_POST['uname']))
             {
                 $unameErr = "User Name must be Filled";
+                $status=false;
+            }
+            else {
+                $uname =$_POST['uname'];
             }
 
             if(empty($_POST['email']))
             {
                 $emailErr = "Email must be Filled";
+                $status=false;
+            }
+            else {
+                $email=$_POST['email'];
             }
 
             if(empty($_POST['num']))
             {
                 $numErr = "Number must be Filled";
+                $status=false;
+            }
+            else {
+                $num=$_POST['num'];
             }
 
             if(empty($_POST['pass1']))
             {
                 $pass1Err = "Password must be Filled";
+                $status=false;
+            }
+            else {
+                $pass1=sha1($_POST['pass1']);
             }
 
             if(empty($_POST['pass2']))
             {
                 $pass2Err = "Password must be Filled";
+                $status=false;
+            }
+            else {
+                $pass2=$_POST['pass2'];
+            }
+
+
+
+            $servername="localhost";
+            $username="root";
+            $password="";
+            $db="blog";
+
+            if($status)
+            {
+                $conn = new mysqli($servername, $username, $password, $db);
+                if($conn -> connect_error)
+                {
+                    die("ERROR: ". $conn->connect_error);
+                }
+
+                $sql = "INSERT INTO users (first_name, last_name, user_name, email, number, password)
+                        values('$fname', '$lname', '$uname', '$email', '$num', '$pass1')";
+
+                        if($conn->query($sql))
+                        {
+                            $msg = "Register Successfull";
+                        }
+                        else {
+                            echo "ERROR". $sql ."<br>". $conn->error;
+                        }
             }
         }
     ?>
     
     <section class="container-form">
-        <form id="my-form" action="" name="login" onsubmit="return validateForm()" method="POST">
+        <p class="val-php"><?php echo $msg; ?></p>
+        <form id="my-form" action="<?=$_SERVER['PHP_SELF'];?>" name="login" onsubmit="return validateForm()" method="POST">
             <h1>Register</h1>
 
             <div>
